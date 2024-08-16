9ae30a788aaadb3c65cafa0b38e7c7734a031169
rayyan ahsan <raygraphicsreal@gmail.com>
2024-08-16 15:07:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+function click() {
+  const button = document.getElementById("queueBtn")
+  button.className = "btn btn-outline-secondary btn-lg disabled"
+  button.textContent = "00:00"
+  var now = new Date().getTime();
+  var x = setInterval(function() {
+
+    // Get today's date and time
+    var live = new Date().getTime();
+  
+    // Find the distance between now and the count down date
+    var distance = live - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2, '0');
+  
+    // Display the result in the element with id="demo"
+    document.getElementById("queueBtn").textContent = minutes + ":" + seconds + "";
+  
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+  fetch("/queue", {
+    method: "POST",
+    body: {"inQueue": "yes"},
+    headers: {"Content-Type": "application/json"}
+  })
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="d-grid gap-2 col-3 mx-auto">
+        <button id="queueBtn" type="button" class="btn btn-success btn-lg btn-change" onClick={click}>Join Queue</button>
+        <p class="text-center">5 people in queue</p>
+        </div>
       </header>
     </div>
   );
